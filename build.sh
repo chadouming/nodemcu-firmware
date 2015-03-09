bc7bcd67bdd02d8c6ca2605adf9faa5ee2acd337
Chad Cormier Roussel <chadcormierroussel@gmail.com>
2015-03-09 13:21:59 +0100

    Fix build script for making single file firmware

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ rm bin/0x00000.bin
 rm bin/0x10000.bin
 mv app/.output/eagle/debug/image/eagle.app.v6.out-0x00000.bin bin/0x00000.bin
 mv app/.output/eagle/debug/image/eagle.app.v6.out-0x10000.bin bin/0x10000.bin
+dd if=bin/0x00000.bin ibs=65k count=100 of=bin/0x00000Padded.bin conv=sync
+cat bin/0x00000Padded.bin bin/0x10000.bin > prebuilt/firmware_latest.bin
