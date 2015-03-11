8d9d4339e23b512e53894ff34d7421bf5994e676
Chad Cormier Roussel <chadcormierroussel@gmail.com>
2015-03-11 22:25:57 +0100

    Remove useless files update build script update prebuilt file

--- build.sh
+++ build.sh
@@ -4,5 +4,8 @@ rm bin/0x00000.bin
 rm bin/0x10000.bin
 mv app/.output/eagle/debug/image/eagle.app.v6.out-0x00000.bin bin/0x00000.bin
 mv app/.output/eagle/debug/image/eagle.app.v6.out-0x10000.bin bin/0x10000.bin
-dd if=bin/0x00000.bin ibs=65k count=100 of=bin/0x00000Padded.bin conv=sync
-cat bin/0x00000Padded.bin bin/0x10000.bin > prebuilt/firmware_latest.bin
+newsize=$((65 * 1024))
+filesize=$(stat -c "%s" bin/0x00000.bin)
+padcount=$((newsize - filesize))
+dd if=/dev/zero ibs=1 count="$padcount" | tr "\000" "\377" >> bin/0x00000.bin
+cat bin/0x00000 bin/0x10000.bin > prebuilt/firmware_latest.bin
