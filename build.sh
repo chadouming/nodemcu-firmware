b9e075f6199d2b6654940e59285ccfd0b131672a
Chad Cormier Roussel <chadcormierroussel@gmail.com>
2015-03-11 22:55:29 +0100

    Yet more attempt to fix single file firmware

--- build.sh
+++ build.sh
@@ -2,10 +2,7 @@ make clobber && make
 esptool.py elf2image app/.output/eagle/debug/image/eagle.app.v6.out
 rm bin/0x00000.bin
 rm bin/0x10000.bin
+rm prebuilt/firmware_latest.bin
 mv app/.output/eagle/debug/image/eagle.app.v6.out-0x00000.bin bin/0x00000.bin
 mv app/.output/eagle/debug/image/eagle.app.v6.out-0x10000.bin bin/0x10000.bin
-newsize=$((65 * 1024))
-filesize=$(stat -c "%s" bin/0x00000.bin)
-padcount=$((newsize - filesize))
-dd if=/dev/zero ibs=1 count="$padcount" | tr "\000" "\377" >> bin/0x00000.bin
-cat bin/0x00000.bin bin/0x10000.bin > prebuilt/firmware_latest.bin
+srec_cat -output "prebuilt/firmware_latest.bin" -binary bin/0x00000.bin -binary -fill 0xff 0x00000 0x10000 bin/0x10000.bin -binary -offset 0x10000
